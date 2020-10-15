249d01af6d64f7be60bd959dc84e4edd4ab08643
yonatan <yendu669@gmail.com>
2020-10-15 15:43:24 +0300

    ViewModels injection implemented

---
+++ app/src/main/java/com/yendu/daggerpractice3/ViewModelsProviderFactory.kt
@@ -0,0 +1,43 @@
+package com.yendu.daggerpractice3
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Singleton
+class ViewModelsProviderFactory @Inject constructor(private val creators: MutableMap<Class<out ViewModel>, Provider<ViewModel>>) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
+        var creator: Provider<ViewModel>? = creators[modelClass];
+        if (creator == null) { // if the viewmodel has not been created
+
+            // loop through the allowable keys (aka allowed classes with the @ViewModelKey)
+            for ((key, value) in creators) {
+
+                // if it's allowed, set the Provider<ViewModel>
+                if (modelClass.isAssignableFrom(key)) {
+                    creator = value
+                    break
+                }
+            }
+        }
+
+        // if this is not one of the allowed keys, throw exception
+        requireNotNull(creator) { "unknown model class $modelClass" }
+
+        // return the Provider
+        return try {
+            creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+
+    companion object {
+        private const val TAG = "ViewModelProviderFactor"
+    }
+
+}
